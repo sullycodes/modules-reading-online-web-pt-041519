e729482b3130181c775d7b8cb98a694b6e62dfb6
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 14:33:16 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,12 +3,12 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer 
-  # include Dance
-  # extend MetaDancing
+  include Dance
+  extend MetaDancing
   
-  extend FancyDance::ClassMethods 
-  include 
-    FancyDance::InstanceMethods
+  # extend FancyDance::ClassMethods 
+  # include 
+  #   FancyDance::InstanceMethods
 
   attr_accessor :name 
   
