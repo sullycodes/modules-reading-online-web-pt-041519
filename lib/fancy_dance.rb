d8790da684896412e96fd895a31019f569c9d95a
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 14:12:45 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,8 +1,27 @@
 module FancyDance 
   module InstanceMethods
   
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+
+  end #InstanceMethods
+
+  module ClassMethods 
     
-  
-  end
+  end #ClassMethods
+
 
 end
